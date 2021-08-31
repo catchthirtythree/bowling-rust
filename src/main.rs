449d6d16a4bb6d2a7d0fa278cdfd306bb8e34082
Michael <hellercmichael@gmail.com>
2021-08-31 12:49:10 -0400

    Clean up some code.

--- src/main.rs
+++ src/main.rs
@@ -35,11 +35,7 @@ fn calculate_frame_score(frame: i32, rolls: &mut VecDeque<i32>) -> i32 {
     let mut current_score: i32 = 0;
 
     for current_frame in 1..=MAX_FRAMES_PER_GAME {
-        if current_frame == MAX_FRAMES_PER_GAME {
-            current_score += calculate_score_last_frame(rolls);
-        } else {
-            current_score += calculate_score_regular_frame(rolls);
-        }
+        current_score += calculate_score_for_frame(current_frame, rolls);
 
         if current_frame == frame {
             break
@@ -53,17 +49,13 @@ fn calculate_score(rolls: &mut VecDeque<i32>) -> i32 {
     let mut current_score: i32 = 0;
 
     for current_frame in 1..=MAX_FRAMES_PER_GAME {
-        if current_frame == MAX_FRAMES_PER_GAME {
-            current_score += calculate_score_last_frame(rolls);
-        } else {
-            current_score += calculate_score_regular_frame(rolls);
-        }
+        current_score += calculate_score_for_frame(current_frame, rolls);
     }
 
     current_score
 }
 
-fn calculate_score_last_frame(rolls: &mut VecDeque<i32>) -> i32 {
+fn calculate_score_for_frame(frame: i32, rolls: &mut VecDeque<i32>) -> i32 {
     let mut frame_score: i32 = 0;
 
     if let Some(roll1) = rolls.pop_front() {
@@ -86,38 +78,12 @@ fn calculate_score_last_frame(rolls: &mut VecDeque<i32>) -> i32 {
                         frame_score += b1;
                     }
 
-                    if let Some(roll3) = rolls.get(0) {
-                        frame_score += roll3;
-                    }
-                }
-            }
-        }
-    }
-
-    frame_score
-}
-
-fn calculate_score_regular_frame(rolls: &mut VecDeque<i32>) -> i32 {
-    let mut frame_score: i32 = 0;
-
-    if let Some(roll1) = rolls.pop_front() {
-        frame_score += roll1;
-
-        if roll1 == MAX_ROLL_SCORE {
-            if let Some(b1) = rolls.get(0) {
-                frame_score += b1;
-            }
-
-            if let Some(b2) = rolls.get(1) {
-                frame_score += b2;
-            }
-        } else {
-            if let Some(roll2) = rolls.pop_front() {
-                frame_score += roll2;
-
-                if roll1 + roll2 == MAX_ROLL_SCORE {
-                    if let Some(b1) = rolls.get(0) {
-                        frame_score += b1;
+                    // In the final frame, a spare will warrant an extra
+                    // roll for the player
+                    if frame == MAX_FRAMES_PER_GAME {
+                        if let Some(roll3) = rolls.get(0) {
+                            frame_score += roll3;
+                        }
                     }
                 }
             }
@@ -222,6 +188,13 @@ mod tests {
         let rolls = [10, 7, 3, 2, 1, 7, 3, 4, 6, 2, 6, 0, 10, 8, 0, 4, 1, 10, 9, 1];
 
         assert_eq!(calculate_score(&mut VecDeque::from_iter(rolls)), 120);
+    }
+
+    #[test]
+    fn test_calculate_score_179_game() {
+        let rolls = [7, 3, 7, 3, 7, 3, 7, 3, 7, 3, 7, 3, 7, 3, 7, 3, 7, 3, 7, 3, 7];
+
+        assert_eq!(calculate_score(&mut VecDeque::from_iter(rolls)), 177);
     }
 
     #[test]
